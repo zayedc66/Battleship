85c0d7b04c288a5b85737ca2cdeb64965285a9e0
zayedc66 <zayedc66@kprschools.ca>
2023-02-10 11:05:32 -0500

    completed random placements and ships

--- main.py
+++ main.py
@@ -6,70 +6,75 @@ import os
 spot=[]
 i=1 
 while i<=25:
-    spot.append(str(i))
+    spot.append("🌊")
+    spot.append("🌊")
+    spot.append("🌊")
+    spot.append("🌊")
+    spot.append("🌊")
     i+=1
-    
-
+#HIDDEN_BOARD=[['']*5 for x in range(5)] 
+#GUESS_BOARD=[['']*5 for x in range(5)] 
 run=True
 while run:
     i=0
     row=1
-    print("___________________________")
+    print("       A    B    C    D    E")
+    print("______________________________________")
     while i<25:
-        print(f"|   {spot[i]}   {spot[i+1]}   {spot[i+2]}   {spot[i+3]}   {spot[i+4]}|")
+        print(f"|{row} |   {spot[i]}  |  {spot[i+1]}  |  {spot[i+2]}  |  {spot[i+3]}  |  {spot[i+4]}|")
         i+=5
         row+=1
-    print("---------------------------")
-    guess = (input("Enter a number from 1 to 25: "))
-    if guess=="1":
-        spot[0]="M"
-    if guess=="2":
-        spot[1]="M"
-    if guess=="3":
-        spot[2]="M"
-    if guess=="4":
-        spot[3]="M"
-    if guess=="5":
-        spot[4]="M"
-    if guess=="6":
-        spot[5]="M"
-    if guess=="7":
-        spot[6]="M"
-    if guess=="8":
-        spot[7]="M"
-    if guess=="9":
-        spot[8]="M"
-    if guess=="10":
-        spot[9]="M"
-    if guess=="11":
-        spot[10]="M"
-    if guess=="12":
-        spot[11]="M"
-    if guess=="13":
-        spot[12]="M"
-    if guess=="14":
-        spot[13]="M"
-    if guess=="15":
-        spot[14]="M"
-    if guess=="16":
-        spot[15]="M"
-    if guess=="17":
-        spot[16]="M"
-    if guess=="18":
-        spot[17]="M"
-    if guess=="19":
-        spot[18]="M"
-    if guess=="20":
-        spot[19]="M"
-    if guess=="21":
-        spot[20]="M"
-    if guess=="22":
-        spot[21]="M"
-    if guess=="23":
-        spot[22]="M"
-    if guess=="24":
-        spot[23]="M"
-    if guess=="25":
-        spot[24]="M"
+    print("--------------------------------------")
+    guess = (input("Enter a coordinate(letter, number): ")).upper()
+    if guess=="A1":
+        spot[0]="💨"
+    if guess=="A2":
+        spot[1]="💨"
+    if guess=="A3":
+        spot[2]="💨"
+    if guess=="A4":
+        spot[3]="💨"
+    if guess=="A5":
+        spot[4]="💨"
+    if guess=="B1":
+        spot[5]="💨"
+    if guess=="B2":
+        spot[6]="💨"
+    if guess=="B3":
+        spot[7]="💨"
+    if guess=="B4":
+        spot[8]="💨"
+    if guess=="B5":
+        spot[9]="💨"
+    if guess=="C1":
+        spot[10]="💨"
+    if guess=="C2":
+        spot[11]="💨"
+    if guess=="C3":
+        spot[12]="💨"
+    if guess=="C4":
+        spot[13]="💨"
+    if guess=="C5":
+        spot[14]="💨"
+    if guess=="D1":
+        spot[15]="💨"
+    if guess=="D2":
+        spot[16]="💨"
+    if guess=="D3":
+        spot[17]="💨"
+    if guess=="D4":
+        spot[18]="💨"
+    if guess=="D5":
+        spot[19]="💨"
+    if guess=="E1":
+        spot[20]="💨"
+    if guess=="E2":
+        spot[21]="💨"
+    if guess=="E3":
+        spot[22]="💨"
+    if guess=="E4":
+        spot[23]="💨"
+    if guess=="E5":
+        spot[24]="💨"
     print(guess)
     os.system("cls")
