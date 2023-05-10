30f8fa866fc6d87389b87e28ca718559d4ce2b0e
zayedc66 <zayedc66@kprschools.ca>
2023-05-10 11:05:17 -0400

    finally

--- customobj/cus.py
+++ customobj/cus.py
@@ -12,33 +12,42 @@ fps = 60
 fpsClock = pygame.time.Clock()
 WINDOW_WIDTH = 500
 WINDOW_HEIGHT = 500
-
+done = False
 
 #Setup of Starting objects
 window = pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT), pygame.HWSURFACE)
 pygame.display.set_caption("Maze")
 walls = pygame.sprite.Group()
 walls.add(Background(0,0,WINDOW_WIDTH,WINDOW_HEIGHT,'maze.png'))
-man_x = 100
-man_y = 100
-speed = 5
+man_x = 24
+man_y = 25
+speed = 3
+font = pygame.font.SysFont('Consolas', 30)
+'''img_portal = pygame.image.load('portal.png')
+img_portal = pygame.transform.scale(img_portal, (25, 25))'''
 
-move_pac = Move_Object(man_x, man_y, 35, 35, 'pac.png')
+
+move_pac = Move_Object(man_x, man_y, 25, 25, 'pac.png')
 move_pac_group = pygame.sprite.Group()
 move_pac_group.add(move_pac)
 
+endobj = Move_Object(455, 455, 25, 25, 'portal.png')
+endobj_group = pygame.sprite.Group()
+endobj_group.add(endobj)
 
-def collision(move_pac_group, walls):
-    return move_pac_group.colliderect(walls)
+def collision(object1, object2):
+    return object1.colliderect(object2)
 
 def display():
-    window.fill((255,255,255)) #White background
+    global portal
+    window.fill((150,150,155)) #White background
     walls.draw(window)
-    move_pac_group.draw(window)   
+    move_pac_group.draw(window)
+    endobj_group.draw(window)
     #gridHelp(window,WINDOW_WIDTH,WINDOW_HEIGHT)  
 
 
-'''def win():
+def win():
     window.blit(font.render("You Win", True, (255,255,255)), (250, 250))
     exit = window.blit(font.render("Exit?", True, (255,255,255)), (250, 200))
     playagn = window.blit(font.render("Play Again?", True, (255,255,255)), (250, 300))
@@ -49,12 +58,14 @@ def display():
                 pygame.quit()
                 sys.exit()
             if playagn.collidepoint(pos):
-                global king_y, king_x, done
-                king_x=10
-                king_y=10
+                global man_y, man_x, done
+                man_x=10
+                man_y=10
                 done = False
                 display()
-'''
+
+
+
 
 def gridHelp(window,WINDOW_WIDTH,WINDOW_HEIGHT):
         spacer = 10
@@ -70,14 +81,19 @@ def gridHelp(window,WINDOW_WIDTH,WINDOW_HEIGHT):
 
 display()
 while True:
+    display()
     for event in pygame.event.get():
       # if user  QUIT then the screen will close
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    if collision(move_pac.rect, endobj.rect):
+        done = True
+        win()      
     move_pac.key_press(5)
     if pygame.sprite.spritecollide(move_pac, walls, False, collided=pygame.sprite.collide_mask):
         move_pac.rect.x-=move_pac.movex
         move_pac.rect.y-=move_pac.movey
+        display()
     pygame.display.update() #update the display
     fpsClock.tick(fps) #speed of redraw
